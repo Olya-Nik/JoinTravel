73f4c65878176726211ec1b80afb555b9db66047
Anita <y_anita@mail.ru>
2019-08-26 09:47:28 +0300

    pull Olga

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const passport = require('passport');
 const MongoStore = require('connect-mongodb-session')(session);
-
+const { User } = require('./models/User');
 
 mongoose.connect('mongodb://localhost:27017/JoinTravel', {
   useNewUrlParser: true
@@ -88,7 +88,7 @@ app.post('/login', (req, res, next) => {
 });
 
 app.post('/signup', (req, res, next) => {
-  console.log(req.body)
+  console.log(req.body);
   passport.authenticate(
     'local-signup',
     { failureFlash: true },
@@ -104,7 +104,7 @@ app.post('/signup', (req, res, next) => {
         if (err) {
           return next(err);
         }
-        res.json({ username: user.username, id: user._id, session: req.session.user});
+        res.json({ username: user.username, id: user._id });
       });
     }
   )(req, res, next);
@@ -115,6 +115,13 @@ app.post('/logout', (req, res) => {
   res.redirect('/');
 });
 
+app.post('/messages', async function(req, res) {
+  const dataMongo = await req.body.data;
+  console.log(dataMongo);
+  const mes = new User({ message: dataMongo });
+  mes.save();
+  res.send(mes);
+});
 
 app.use('/', indexRouter);
 app.listen(3001, function() {
